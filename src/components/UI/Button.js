fb04a90b01de65ca6621b1c93643a98360883187
Frienchiese <batuhan.yildiz113@gmail.com>
2022-10-03 16:17:56 +0300

    finito

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -4,7 +4,10 @@ import classes from './Button.module.css';
 
 const Button = (props) => {
     return (
-        <button className={classes.button} > {props.children} </button>
+        <button 
+        className={classes.button}
+        onClick={props.onClick}
+         > {props.children} </button>
     )
 };
 
--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -8,10 +8,22 @@ import classes from './AddUser.module.css'
 const AddUsers = (props) => {
     const [enteredUsername, setEnteredUsername] = useState("");
     const [enteredAge, setEnteredAge] = useState("");
+    const [error, setError] = useState("");
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0 || +enteredAge < 0) {
+        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                title: "Invalid input value",
+                message: "Please fill all the inputs."
+            })
+            return;
+        }
+        if (+enteredAge < 0) {
+            setError({
+                title: "Invalid age input value",
+                message: "Please enter valid age value (> 0)"
+            })
             return;
         }
         props.onAddUser(enteredUsername, enteredAge);
@@ -27,10 +39,16 @@ const AddUsers = (props) => {
         setEnteredAge(event.target.value);
     }
 
+    const closeModal = () => {
+        setError(null);
+    }
 
     return (
         <div>
-            <ErrorModal title="An error occured" message="Something went wrong"></ErrorModal>
+            {error && <ErrorModal 
+            title={error.title} 
+            message={error.message} 
+            onHandleError={closeModal} />}
             <Card className={classes.input} >
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username" >Username</label>
